07acb005f821e36e39746a6893a4b362577a285f
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-07 20:10:02 -0600

    Creando el repositorio JPA para los productos

---
+++ src/main/java/com/aboylan/springboot/app/productos/models/dao/ProductoDao.java
@@ -0,0 +1,9 @@
+package com.aboylan.springboot.app.productos.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.aboylan.springboot.app.productos.models.entity.Producto;
+
+public interface ProductoDao extends CrudRepository<Producto, Long> {
+
+}

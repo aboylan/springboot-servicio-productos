7bae066072bf6cf40eb1312b4c736e3e1f77fc45
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-10 10:15:06 -0600

    Tolerancia de fallos y latencia con Hystrix

--- src/main/java/com/aboylan/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/aboylan/springboot/app/productos/controllers/ProductoController.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +15,8 @@ import com.aboylan.springboot.app.productos.models.service.IProductoService;
 @RestController
 public class ProductoController {
 
-	@Autowired
-	private Environment env;
+	//@Autowired
+	//private Environment env;
 
 	@Value("${server.port}")
 	private Integer port;
@@ -39,6 +38,7 @@ public class ProductoController {
 		Producto producto = productoService.findById(id);
 		// producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		producto.setPort(port);
+		
 		return producto;
 	}
 }

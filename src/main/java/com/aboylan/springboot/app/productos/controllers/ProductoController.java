273bc80a8fa56a965570c050397944200b7434b5
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-11 21:37:43 -0600

    Configurando timeout en Hystrix y Ribbon

--- src/main/java/com/aboylan/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/aboylan/springboot/app/productos/controllers/ProductoController.java
@@ -39,6 +39,12 @@ public class ProductoController {
 		// producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		producto.setPort(port);
 		
+		/*try {
+			Thread.sleep(2000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+		
 		return producto;
 	}
 }
